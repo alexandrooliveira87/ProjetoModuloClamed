fe1161761614151884ead39ea9be5c018c342622
Alexandro Oliveira <=>
2024-10-22 11:31:39 -0300

    Criação da tela de login

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from '../types';
+
+type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
+
+interface Props {
+  navigation: HomeScreenNavigationProp;
+}
+
+const HomeScreen: React.FC<Props> = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem-vindo à Home</Text>
+      <Button title="Sair" onPress={() => navigation.navigate('Login')} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+});
+
+export default HomeScreen;
